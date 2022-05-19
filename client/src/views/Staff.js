f15dba1c13603e8040ab1d566506e00349c15028
Muhammad Rehan <75864401+MuhammadRehanRasool@users.noreply.github.com>
2022-05-19 15:21:05 -0700

    CHNAGES

--- client/src/views/Staff.js
+++ client/src/views/Staff.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { CONSTANT, Loader } from "./../CONSTANT";
 import { useNavigate } from "react-router-dom";
 import { QRCodeCanvas } from "qrcode.react";
-import { QrReader } from 'react-qr-reader';
+import { QrReader } from "react-qr-reader";
 const axios = require("axios");
 
 function Staff(props) {
@@ -14,20 +14,20 @@ function Staff(props) {
   // User Data
 
   const [scanOpen, setScanOpen] = useState(false);
-  const handleErrorWebCam = (error) => {
-    console.log(error);
-  }
-  const handleScanWebCam = (result) => {
-    console.log(result)
-    if (result) {
-      console.log(result)
+  const [QR, setQR] = useState("");
+
+  useEffect(() => {
+    if (QR !== "") {
+      setScanOpen(!scanOpen);
     }
-  }
+  }, [QR]);
 
   return (
     <div className="__Staff">
       <div className="row d-flex flex-column justify-content-center align-items-center">
-        <h1 className="mb-4 mt-2 text-center">Welcome {data.personal.username}!</h1>
+        <h1 className="mb-4 mt-2 text-center">
+          Welcome {data.personal.username}!
+        </h1>
         <h3 className="mb-4 mt-2 text-center">Scan QR Code</h3>
         <div className="w-50 mt-3 custom-button text-center">
           <button
@@ -38,19 +38,30 @@ function Staff(props) {
             }}
             onClick={() => {
               setScanOpen((scanOpen) => {
-                return !scanOpen
-              })
+                return !scanOpen;
+              });
             }}
           >
             {scanOpen ? "Close" : "Open"} Scanner
           </button>
-          {scanOpen ? <QrReader
-            delay={300}
-            style={{ width: '100%' }}
-            onError={handleErrorWebCam}
-            onScan={handleScanWebCam}
-            className="scanner"
-          /> : ""}
+          {scanOpen ? (
+            <QrReader
+              delay={300}
+              style={{ width: "100%" }}
+              onResult={(result, error) => {
+                if (!!result) {
+                  setQR(result?.text);
+                }
+
+                if (!!error) {
+                  console.info(error);
+                }
+              }}
+              className="scanner"
+            />
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
